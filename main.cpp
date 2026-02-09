a1fb956111a4eb603f5c51740ea73798570c0492
|GriB28| <gp.off.dev@gmail.com>
2026-02-10 00:16:52 +0300

    fixed syntax + added a font

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/Graphics.hpp>
 
 using sf::RenderWindow;
 using sf::Text;
+using sf::Font;
+using sf::VideoMode;
+using sf::Vector2f;
 using std::to_string;
 
 int main() {
     unsigned long long frames = 0;
 
     RenderWindow window(
-        sf::VideoMode({1280, 720}),
+        VideoMode({1280, 720}),
         "SFML-built-test"
     );
     window.setFramerateLimit(24);
 
-    Text frames_text();
+    Font font;
+    if (!font.openFromFile("Kuro Regilar.otf")) return 1;
+
+    Text frames_text(font);
     frames_text.setCharacterSize(10);
-    frames_text.setPosition(620, 355);
+    frames_text.setPosition(Vector2f{620, 355});
 
     while (window.isOpen()) {
         window.clear();
